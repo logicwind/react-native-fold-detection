b2312c07d0cf2d1387c3989f15d4bb1c0758618b
Bhavesh <bhavesh@Bhaveshs-MacBook-Pro.local>
2024-12-11 11:14:36 +0530

    fix: ios build issue on RN 76

--- ios/FoldDetection.h
+++ ios/FoldDetection.h
@@ -1,12 +1,4 @@
-
-#ifdef RCT_NEW_ARCH_ENABLED
-#import "RNFoldDetectionSpec.h"
-
-@interface FoldDetection : NSObject <NativeFoldDetectionSpec>
-#else
 #import <React/RCTBridgeModule.h>
 
 @interface FoldDetection : NSObject <RCTBridgeModule>
-#endif
-
 @end
